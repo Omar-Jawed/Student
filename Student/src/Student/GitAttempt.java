85640018197853eddc757f4a28c45ffa47640751
Omar Jawed <jawedsy@sheridancollege.ca>
2020-01-15 14:52:03 -0500

    Works

--- Student/src/Student/GitAttempt.java
+++ Student/src/Student/GitAttempt.java
@@ -4,6 +4,6 @@ public class GitAttempt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("How does this work??");
+		System.out.println("This works");
 	}
 }
