cbde340318dda5766c8dd4e6b48365974be30bd9
Omar-Jawed <59933639+Omar-Jawed@users.noreply.github.com>
2020-01-22 13:34:17 -0500

    Update GitAttempt.java

--- Student/src/Student/GitAttempt.java
+++ Student/src/Student/GitAttempt.java
@@ -4,6 +4,6 @@ public class GitAttempt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Did this work?");
+		System.out.println("Changed");
 	}
 }
