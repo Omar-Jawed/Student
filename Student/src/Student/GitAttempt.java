ef2c16dbc3c0bd74e5bd9879fec89afd246dcf4e
Omar Jawed <jawedsy@sheridancollege.ca>
2020-01-22 13:54:48 -0500

    a

--- Student/src/Student/GitAttempt.java
+++ Student/src/Student/GitAttempt.java
@@ -2,9 +2,15 @@ package Student;
 
 public class GitAttempt {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		// TODO Auto-generated method stub
 		System.out.println("Yes");
 		System.out.println("Changed");
 		System.out.println("OK line");
+=======
+		// TODO Auto-generated method stub
+		System.out.println("Yes");
+		System.out.println("Omar Jawed");
+>>>>>>> refs/remotes/origin/master
 	}
 }
