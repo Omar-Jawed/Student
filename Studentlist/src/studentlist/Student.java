c687f740173201f7de023591c09762a261113790
Omar Jawed <jawedsy@sheridancollege.ca>
2020-01-22 14:43:21 -0500

    Formatting

--- Studentlist/src/studentlist/Student.java
+++ Studentlist/src/studentlist/Student.java
@@ -4,44 +4,37 @@
  * and open the template in the editor.
  */
 package studentlist;
-
 /**
  *
  * @author srinivsi
  * @date
  */
-public class Student
-{
-    private String name;
-    private int age;
-    private int partid;
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the age
-     */
-    public int getAge() {
-        return age;
-    }
-
-    /**
-     * @param age the age to set
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
-    
+public class Student {
+	private String name;
+	private int age;
+	private int partid;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
